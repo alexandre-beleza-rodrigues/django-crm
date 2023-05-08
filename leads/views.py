6331a606ff748be31da2febbb7c6054ae573468a
Alexandre Rodrigues <alexandrebelezarodrigues@gmail.com>
2023-05-08 09:21:13 +0100

    Add lead form

--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .models import Lead
+from .forms import LeadForm
 
 
-def home_page(request):
-    return render(request, "home.html")
+def lead_list(request):
+    _leads = Lead.objects.all()
+    context = {"leads": _leads}
+    return render(request, "lead_list.html", context)
+
+
+def lead_detail(request, pk):
+    _lead = Lead.objects.get(id=pk)
+    context = {"lead": _lead}
+    return render(request, "lead_detail.html", context)
+
+
+def lead_create(request):
+    context = {
+        "form": LeadForm()
+    }
+    return render(request, "lead_create.html", context)
