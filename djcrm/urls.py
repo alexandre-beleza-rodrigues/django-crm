1a885f1a0ab98d86e5d0437b1f0fc80fcc94cec5
Alexandre Rodrigues <alexandrebelezarodrigues@gmail.com>
2023-05-23 18:46:06 +0100

    Implement reset password

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,8 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetCompleteView,
+    PasswordResetConfirmView,
+    PasswordResetDoneView,
+    PasswordResetView,
+)
 from django.urls import include, path
-from django.contrib.auth.views import LoginView, LogoutView
 
 from leads.views import LandingPageView, SinupView
 
@@ -12,6 +19,22 @@ urlpatterns = [
     path("signup/", SinupView.as_view(), name="signup"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("reset-password/", PasswordResetView.as_view(), name="reset-password"),
+    path(
+        "password-reset-done/",
+        PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset-complete/",
+        PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
     path("leads/", include("leads.urls", namespace="leads")),
     path("agents/", include("agents.urls", namespace="agents")),
 ]
