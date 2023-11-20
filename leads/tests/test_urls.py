2d9452983f522f7549cf386b3152015bcbd84505
Alexandre Rodrigues <alexandrebelezarodrigues@gmail.com>
2023-11-20 09:08:52 +0000

    Add some more leads app URL tests

--- leads/tests/test_urls.py
+++ leads/tests/test_urls.py
@@ -1,15 +1,20 @@
-from django.test import SimpleTestCase
 from django.urls import reverse, resolve
+from leads.tests import CRMTestCase
 from leads.views import (
     LeadListView,
     LeadDetailView,
     LeadCreateView,
     LeadUpdateView,
     LeadDeleteView,
+    CategoryListView,
+    CategoryCreateView,
+    CategoryDetailView,
+    CategoryUpdateView,
+    CategoryDeleteView,
 )
 
 
-class TestUrls(SimpleTestCase):
+class TestUrls(CRMTestCase):
     def test_lead_list_url_resolves(self):
         url = reverse("leads:lead-list")
         self.assertEqual(resolve(url).func.view_class, LeadListView)
@@ -29,3 +34,23 @@ class TestUrls(SimpleTestCase):
     def test_lead_delete_url_resolves(self):
         url = reverse("leads:lead-delete", args=[1])
         self.assertEqual(resolve(url).func.view_class, LeadDeleteView)
+
+    def test_category_list_url_resolves(self):
+        url = reverse("leads:category-list")
+        self.assertEqual(resolve(url).func.view_class, CategoryListView)
+
+    def test_category_create_url_resolves(self):
+        url = reverse("leads:category-create")
+        self.assertEqual(resolve(url).func.view_class, CategoryCreateView)
+
+    def test_category_detail_url_resolves(self):
+        url = reverse("leads:category-detail", args=[self.default_category.pk])
+        self.assertEqual(resolve(url).func.view_class, CategoryDetailView)
+
+    def test_category_update_url_resolves(self):
+        url = reverse("leads:category-update", args=[self.default_category.pk])
+        self.assertEqual(resolve(url).func.view_class, CategoryUpdateView)
+
+    def test_category_delete_url_resolves(self):
+        url = reverse("leads:category-delete", args=[self.default_category.pk])
+        self.assertEqual(resolve(url).func.view_class, CategoryDeleteView)
