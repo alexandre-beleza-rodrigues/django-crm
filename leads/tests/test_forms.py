dbd50e6ac4c684fe5caeff83b4cd3223c1ecef2b
Alexandre Rodrigues <alexandrebelezarodrigues@gmail.com>
2023-11-20 08:32:44 +0000

    Add UserCreationForm tests

--- leads/tests/test_forms.py
+++ leads/tests/test_forms.py
@@ -1,9 +1,41 @@
-from django.test import TestCase
-from leads.forms import LeadModelForm
+from leads.tests import CRMTestCase
+from leads.forms import LeadModelForm, UserCreationForm
 from leads.models import Agent, User, Category
 
 
-class TestLeadModelForm(TestCase):
+class TestUserCreationForm(CRMTestCase):
+    valid_form_data = {
+        "username": "newuser",
+        "password1": "v3rys3cur3pw",
+        "password2": "v3rys3cur3pw",
+    }
+
+    def test_accepts_valid_data(self):
+        form = UserCreationForm(self.valid_form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_rejects_blank_username(self):
+        data = self.valid_form_data.copy()
+        data["username"] = None
+        form = UserCreationForm(data)
+        self.assertFalse(form.is_valid())
+
+    def test_rejects_blank_password(self):
+        data = self.valid_form_data.copy()
+        data["password1"] = None
+        data["password2"] = None
+        form = UserCreationForm(data)
+        self.assertFalse(form.is_valid())
+
+    def test_rejects_not_matching_passwords(self):
+        data = self.valid_form_data.copy()
+        data["password1"] = "v3rys3cur3pw"
+        data["password2"] = "notmatching"
+        form = UserCreationForm(data)
+        self.assertFalse(form.is_valid())
+
+
+class TestLeadModelForm(CRMTestCase):
     def setUp(self):
         self.default_user = User.objects.create_user(
             username="testuser", password="testpass"
