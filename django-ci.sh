04e0af5850421c6b01138495d09330fbb6392cbc
Alexandre Rodrigues <alexandrebelezarodrigues@gmail.com>
2023-11-23 19:56:47 +0000

    Reduce mandatory code coverage
    Reduced the mandatory code coverage to 98%
    because at the moment the investement to keep
    100% coverage is not worth it.

--- django-ci.sh
+++ django-ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 coverage run --source='.' manage.py test --verbosity=2
-coverage report --fail-under=100
+coverage report --fail-under=98
 coverage html
 pylint $(git ls-files '*.py')
