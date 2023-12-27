799e97ee5bebac259733b4a8d7540466fa1feac6
Alexandre Rodrigues <alexandrebelezarodrigues@gmail.com>
2023-12-27 22:49:02 +0000

    Small update to `manage.sh`

--- manage.sh
+++ manage.sh
@@ -2,9 +2,9 @@
 
 export DEVELOPMENT=True
 
-if [ "$1" = "test" ]; then
+if [ "$1" = "test-lint" ]; then
     shift  # so that `test` is not passed to `manage.py test`
-    coverage run --source='.' manage.py test $@
+    coverage run --source='.' manage.py test --failfast $@
     coverage report --fail-under=98
     coverage html
     pylint $(git ls-files '*.py')
